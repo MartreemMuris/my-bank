872823ffbaa03ff8dc6fad9df29cc57f2d7b0b63
Abanoub Zaki <93231341+AbanoubZaki1@users.noreply.github.com>
2023-06-21 21:11:49 +0300

    Removed some comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_bank/app.dart';
-import 'package:my_bank/common/modules/user.dart';
-import 'package:my_bank/services/database_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // await DatabaseService.instance.updateUser(
-  //   User(
-  //     id: "9",
-  //     name: "Sara",
-  //     email: "Sara@gmail.com",
-  //     currentBalance: 5000,
-  //   ),
-  // );
-
   runApp(const MyApp());
 }
